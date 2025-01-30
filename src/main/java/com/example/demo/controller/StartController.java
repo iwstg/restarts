15bd0966467500b1baa223f15af447beb8e71191
iwstg <iwstg@naver.com>
2025-01-30 19:44:23 +0900

    [2025-01-30] 회원가입 DB 연결 및 구현

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.demo.data.dto.RegisterPageDTO;
+import com.example.demo.service.userControlService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StartController {
+
+    userControlService userService;
+
+    @Autowired
+    public StartController(userControlService userService){
+        this.userService=userService;
+    }
 
     /**
      * [2025-01-14] 메인페이지를 로그인페이지로 리다이랙션 해주는 곳
@@ -34,11 +45,16 @@ public class StartController {
      * [2025-01-14] 회원가입페이지에서 전달받은 form 의 내용을 dto에 맞게 전달받음.
      * 해당 기능은 form에서 받아온 내용으로 회원가입 dao 필요.
      * #option 패스워드 검증 프론트엔드도 추가
+     *
+     * [2025-01-30] 들어온 정보를 비교하여 회원가입에 성공 시 성공팝업 실패시 실패팝업 추가 필요
      */
     @PostMapping("RegisterRequest")
     public String runRegisterRequest(RegisterPageDTO form) {
         System.out.println(form.toString());
         // form 에서 받아온 DTO로 회원가입 서비스 진행
+        userService.userRegistService(form);
+        System.out.println("[Controller] form 데이터 전송 ( Controller -> Service");
+        // 들어온 데이터가 정상이고 처리도 정상으로 됐으면 안내페이지로
         return "LoginPage";
     }
 
--- src/main/java/com/example/demo/data/dto/RegisterPageDTO.java
+++ src/main/java/com/example/demo/data/dto/RegisterPageDTO.java
@@ -6,18 +6,50 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterPageDTO {
-    private String userId;
-    private String userName;
-    private String userEmail;
-    private String userPassword;
+    private String UserId;
+    private String UserName;
+    private String UserEmail;
+    private String UserPassword;
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
 
     @Override
     public String toString() {
         return "RegisterPageDTO{" +
-                "userId='" + userId + '\'' +
-                ", userName='" + userName + '\'' +
-                ", userEmail='" + userEmail + '\'' +
-                ", userPassword='" + userPassword + '\'' +
+                "UserId='" + UserId + '\'' +
+                ", UserName='" + UserName + '\'' +
+                ", UserEmail='" + UserEmail + '\'' +
+                ", UserPassword='" + UserPassword + '\'' +
                 '}';
     }
 }
